f7c6a27299698067c2e5acc5ceb835c879bf5434
Paulo Gabriel <pmedeiros@findes.org.br>
2023-10-06 17:53:34 -0300

    validação de cpf

--- src/funcionario/funcionario.service.ts
+++ src/funcionario/funcionario.service.ts
@@ -8,7 +8,13 @@ export class FuncionarioService {
   constructor(private prisma: PrismaService){}
 
   async create(data:funcionarioDTO){
-    //função cpf
+
+    const validador = isValidCPF(data.cpf)
+    
+    if(!validador){
+      throw new Error('CPF invalido');
+    }
+
     const cpfExist = await this.prisma.funcionario.findFirst({where:{cpf:data.cpf}});
 
     if(cpfExist){
@@ -64,4 +70,21 @@ export class FuncionarioService {
     return await this.prisma.funcionario.delete({where:{id}});
   };
 
+}
+
+function isValidCPF(value: string) {
+  if (typeof value !== 'string') {
+  return false;
+}
+ 
+value = value.replace(/[^\d]+/g, '');
+
+if (value.length !== 11 || !!value.match(/(\d)\1{10}/)) {
+  return false;
+}
+
+const values = value.split('').map(el => +el);
+const rest = (count) => (values.slice(0, count-12).reduce( (soma, el, index) => (soma + el * (count-index)), 0 )*10) % 11 % 10;
+
+return rest(10) === values[9] && rest(11) === values[10];
 }
